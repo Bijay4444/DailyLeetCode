def569cbab14118614dfa5d05d27b6f391c522ad
Bijay Khadka <bk006822@gmail.com>
2024-08-14 21:43:00 +0545

    Update day1.py

--- day1.py
+++ day1.py
@@ -9,13 +9,11 @@ Nary-Tree input serialization is represented in their level order traversal,
 each group of children is separated by the null value (See examples).
 """
 
-"""
 # Definition for a Node.
 class Node(object):
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
-"""
 
 class Solution(object):
     def maxDepth(self, root):
