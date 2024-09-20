23af9fc3b2f1f8befb889c5a1ebb521e143ae42f
Bijay Khadka <bk006822@gmail.com>
2024-09-20 22:02:10 +0545

    SearchInsertPsn:Optimized

--- day14.py
+++ day14.py
@@ -8,10 +8,15 @@ You must write an algorithm with O(log n) runtime complexity.
 """
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)  
+        l, r = 0, len(nums) - 1
         
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+                
+        return l
