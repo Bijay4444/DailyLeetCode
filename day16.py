bc78bcd64e28af64108cb8aba526e7b18facd0e9
Bijay Khadka <bk006822@gmail.com>
2024-09-25 21:01:42 +0545

    brainstromming:day16

--- day16.py
+++ day16.py
@@ -6,6 +6,27 @@ in left-to-right order. The large integer does not contain any leading 0's.
 
 Increment the large integer by one and return the resulting array of digits.
 """
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+digits = [1, 2, 9]
+
+# for i in digits:
+    # if digits[i] <= 9:
+    #     digits[-1] + 1
+updated= digits[-1] + 1
+digits.pop(-1)
+digits.append(updated)
+print(digits)
+
+for i in digits:
+    if i >= 9:
+        digits.pop(-1)
+        updated = 0
+        digits.append(updated)
+        print(digits)
+        
+        digits.pop(-2)
+        updated = digits[-2]+ 1
+        digits[-2] = updated
+        print(digits)
+
+print(digits)
         
