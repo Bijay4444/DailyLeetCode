22a5a02093b79f2a7e7ee8e9f4b266e34b976416
Bijay Khadka <bk006822@gmail.com>
2024-09-26 21:49:21 +0545

    unoptimized:plusone

--- day16.py
+++ day16.py
@@ -6,27 +6,15 @@ in left-to-right order. The large integer does not contain any leading 0's.
 
 Increment the large integer by one and return the resulting array of digits.
 """
-digits = [1, 2, 9]
-
-# for i in digits:
-    # if digits[i] <= 9:
-    #     digits[-1] + 1
-updated= digits[-1] + 1
-digits.pop(-1)
-digits.append(updated)
-print(digits)
-
-for i in digits:
-    if i >= 9:
-        digits.pop(-1)
-        updated = 0
-        digits.append(updated)
-        print(digits)
+def plusone(digits):
+    for i in range(len(digits) - 1, -1, -1):
+        digits[i] += 1
         
-        digits.pop(-2)
-        updated = digits[-2]+ 1
-        digits[-2] = updated
-        print(digits)
-
-print(digits)
+        if digits[i] == 10:
+            digits[i] = 0
+        else:
+            return digits
         
+    return [1] + digits
+
+print(plusone(digits=[9,9,0]))
